08362e2af2c3e62ccbcd2ef3afde0594e31bfe07
Maikon Ferreira <me@maikonferreira.tech>
2021-07-25 18:53:14 -0300

    comment

--- src/processor/processor-views.js
+++ src/processor/processor-views.js
@@ -7,6 +7,7 @@ module.exports = class ProcessorViews {
     this.processViews();
   };
 
+  /** Init and set views. */
   processViews = () => {
     this.handleHomeView();
     this.handleCloudFunctionsView();
